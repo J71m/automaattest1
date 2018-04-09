25e13b8b743a4c89e337c0b45f0009f181a937f1
j71m <23724760+J71m@users.noreply.github.com>
2018-04-09 08:47:07 +0300

    basically done

---
+++ pillart.ee/src/main/java/testimine/pillart/ee/Testing1.java
@@ -0,0 +1,16 @@
+package testimine.pillart.ee;
+import org.openqa.selenium.By;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Condition.*;
+
+
+
+public class Testing1 {
+    public static void main(String[] args){
+
+        System.out.println("Just testing");
+    }
+
+
+}
+
---
+++ pillart.ee/src/test/java/testimine/pillart/ee/PillartTest.java
@@ -0,0 +1,51 @@
+package testimine.pillart.ee;
+import org.junit.*;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.openqa.selenium.By;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Condition.*;
+
+public class PillartTest {
+    /*
+    public PillartTest() {
+    }*/
+
+    
+     @Test
+     public void UITest() {
+        open("http://www.pillart.ee/");
+        $("#a_SysTopMenuTPL_213").click();
+        $("h1").shouldHave(text("Teenus"));       
+        $("#a_SysTopMenuTPL_157").click();        
+        $("#left-side").shouldHave(text("Hea külaline,"));
+        $("#a_SysTopMenuTPL_").click();
+        $(".haldus_txtlink").shouldHave(text("Kasutajanimi"));
+     }
+
+     @Test
+     public void Login(){
+        open("http://www.pillart.ee/");
+        $("#a_SysTopMenuTPL_").click();
+        $("#Kasutajanimi").setValue("deyocito@eth2btc.info");
+        $("#Kasutajanimi").click();
+        $("#Parool").setValue("asdasd");
+        $("#Kasutajanimi").pressEnter();
+        $("#Navigation").shouldHave(text("Lahku"));
+     }
+     
+     @Test
+     public void FailTest(){
+        open("http://www.pillart.ee/");
+        $("#a_SysTopMenuTPL").click();
+     }
+     
+     @Test
+     public void Shopping(){
+         open("http://www.pillart.ee/");
+     }
+}
